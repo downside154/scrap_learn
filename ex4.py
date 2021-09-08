bcbb3f9f8e668b736bc9b66ec45435a713630b40
downside154 <75163801+downside154@users.noreply.github.com>
2021-09-08 17:04:53 +0900

    feat: created file for targeting images
    used developer tool and beautifulsoup.find parameters to target wanted
    element

---
+++ ex4.py
@@ -0,0 +1,13 @@
+#Using Chrome's developer tools to target something
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Seoul_Metropolitan_Subway"
+resp = requests.get(url)
+html_src = resp.text
+
+soup = BeautifulSoup(html_src, 'html.parser')
+first_img = soup.find(name='img')
+print(first_img)
+target_img = soup.find(name='img', attrs={'width':'1000'})
+print(target_img)
