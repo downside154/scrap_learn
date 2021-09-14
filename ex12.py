de5c67db7ce5ff62717dfc1acfe8216739ad2c9a
downside154 <75163801+downside154@users.noreply.github.com>
2021-09-14 19:02:43 +0900

    feat: added explanatory commentary for code

--- ex12.py
+++ ex12.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from decouple import config 
 
+# using selenium for dynamic web scape part
+
 driver = webdriver.Chrome('../scrapping2/chromedriver')
 driver.implicitly_wait(5)
 driver.get("https://www.danawa.com/")
 
 
 login = driver.find_element_by_css_selector('#danawa_menubar > div.main-header__user > div:nth-child(5) > a')
-print("HTML ELEMENT : ", login)
-print(" Tag name : ", login.tag_name )
-print("string : ", login.text )
-print(" href element: ", login.get_attribute('href') )
+print("HTML ELEMENT : ", login) #selenium.webdriver.remote.webelement.WebElement(.......)
+print(" Tag name : ", login.tag_name ) #a
+print("string : ", login.text ) # 로그인 
+print(" href element: ", login.get_attribute('href') )#  href element:  https://auth.danawa.com/login?url=http%3A%2F%2Fwww.danawa.com%2F
 
 
 login.click()
